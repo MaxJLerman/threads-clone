0890c3f0bb15244673817e9bc12c57700c4a2442
Max Lerman <mjl82601@gmail.com>
2023-08-07 14:21:08 +0100

    fixed main page styling

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 import "../globals.css";
 import Topbar from "@/components/shared/Topbar";
@@ -10,7 +11,7 @@ import RightSidebar from "@/components/shared/RightSidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Threads",
   description: "A Next.js Threads clone application",
 };
@@ -21,11 +22,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
-          <main>
+          <main className="flex flex-row">
             <LeftSidebar />
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
